801a11dc8135549f133697de29b5c7da5c73b7aa
Anna Grzejszczak <grzejszczak.anna@gmail.com>
2022-02-02 20:10:43 +0100

    zadanie trzecie Anna Grzejszczak

---
+++ JagiellonianDynasty.pl
@@ -0,0 +1,308 @@
+%Atoms
+
+wladyslawIIJagiello.
+elzbietaBonifacja.
+jadwigaJagiellonka1.
+wladyslawIIIWarnenczyk.
+kazimierz.
+kaziemierzIVJagiellonczyk.
+wladyslawIIJagiellonczyk.
+jadwigaJagiellonka2.
+kaziemierzJagiellonczyk.
+janIolbracht.
+aleksanderJagiellonczyk.
+zofiaJagiellonka1.
+elzbietaJagiellonkaI.
+annaJagiellonka1.
+ludwikIIJagiellonczyk.
+zygmuntIstary.
+fryderykJagiellonczyk.
+elzbietaJagiellonkaII.
+annaJagiellonka2.
+barbaraJagiellonka.
+elzbietaJagiellonkaIII.
+jadwigaJagiellonka3.
+anna.
+izabelaJagiellonka.
+zygmuntIIAugust.
+zofiaJagiellonka2.
+annaJagiellonka3.
+katarzynaJagiellonka.
+wojciechOlbracht.
+jadwigaAndegawenska.
+annaCylejska.
+elzbietaGrabowska.
+zofiaHolszanska.
+elzbietaRakuszanka.
+barbaraHohenzollern.
+beatryczeAragonska.
+annaDeFoix.
+helenamoskiewska.
+barbarZapolya.
+bonaSforzaDaragona.
+mariaHasburzanka.
+elzbietaHabsburzanka.
+barbaraRadziwillowna.
+katarzynaHabsburzanka.
+jerzyBogaty.
+fryderykStarszyHohenzollern.
+ferdynandIHabsburg.
+boguslawX.
+jerzySaski.
+fryderykII.
+joahimIIHektor.
+janZapolyia.
+henrykII.
+stefanBatory.
+janIIIWaza.
+
+
+%Facts
+ 
+man(wladyslawIIJagiello).
+man(wladyslawIIIWarnenczyk).
+man(kazimierz).
+man(kaziemierzIVJagiellonczyk).
+man(wladyslawIIJagiellonczyk).
+man(kaziemierzJagiellonczyk).
+man(janIOlbracht).
+man(aleksanderJagiellonczyk).
+man(ludwikIIJagiellonczyk).
+man(zygmuntIstary).
+man(fryderykJagiellonczyk).
+man(zygmuntIIAugust).
+man(wojciechOlbracht).
+man(jerzyBogaty).
+man(fryderykStarszyHohenzollern).
+man(ferdynandIHabsburg).
+man(boguslawX).
+man(jerzySaski).
+man(fryderykII).
+man(joahimIIHektor).
+man(janZapolyia).
+man(henrykII).
+man(stefanBatory).
+man(janIIIWaza).
+
+woman(elzbietaBonifacja).
+woman(jadwigaJagiellonka1).
+woman(jadwigaJagiellonka2).
+woman(zofiaJagiellonka1).
+woman(elzbietaJagiellonkaI).
+woman(annaJagiellonka1).
+woman(elzbietaJagiellonkaII).
+woman(annaJagiellonka2).
+woman(barbaraJagiellonka).
+woman(elzbietaJagiellonkaIII).
+woman(jadwigaJagiellonka).
+woman(anna).
+woman(izabelaJagiellonka).
+woman(zofiaJagiellonka).
+woman(annaJagiellonka3).
+woman(katarzynaJagiellonka).
+woman(jadwigaAndegawenska).
+woman(annaCylejska).
+woman(elzbietaGrabowska).
+woman(zofiaHolszanska).
+woman(elzbietaRakuszanka).
+woman(barbaraHohenzollern).
+woman(beatryczeAragonska).
+woman(annaDeFoix).
+woman(helenamoskiewska).
+woman(barbarZapolya).
+woman(bonaSforzaDaragona).
+woman(mariaHasburzanka).
+woman(elzbietaHabsburzanka).
+woman(barbaraRadziwillowna).
+woman(katarzynaHabsburzanka).
+
+/*  date (name, birth, death)  */
+
+date(wladyslawIIJagiello, 1362, 1-6-1434).
+date(wladyslawIIIWarnenczyk, 31-10-1424, 10-11-1444).
+date(kazimierz, 16-05-1426, 2-3-1427).
+date(kaziemierzIVJagiellonczyk, 30-10-1427, 7-6-1492).
+date(wladyslawIIJagiellonczyk, 1-3-1456, 13-3-1516).
+date(kaziemierzJagiellonczyk, 3-10-1458, 4-3-1484).
+date(janIOlbracht, 27-12-1459, 17-06-1501).
+date(aleksanderJagiellonczyk, 5-8-1461, 19-8-1506).
+date(ludwikIIJagiellonczyk, 1-7-1506, 29-8-1526).
+date(zygmuntIstary, 1-1-1467, 1-4-1548).
+date(fryderykJagiellonczyk, 27-4-1468, 14-3-1503).
+date(zygmuntIIAugust, 1-8-1520, 7-7-1572).
+date(wojciechOlbracht, 20-9-1527, 20-9-1527).
+date(elzbietaBonifacja, 22-6-1399, 13-7-1399).
+date(jadwigaJagiellonka1, 8-4-1408, 8-12-1431).
+date(jadwigaJagiellonka2, 21-9-1457, 18-2-1502).
+date(zofiaJagiellonka1, 6-5-1464, 5-11-1512).
+date(elzbietaJagiellonkaI, 9-5-1465, 9-5-1466).
+date(annaJagiellonka1, 23-7-1503, 27-1-1547).
+date(elzbietaJagiellonkaII, 13-5-1472, 1480).
+date(annaJagiellonka2, 12-3-1476, 12-8-1503).
+date(barbaraJagiellonka, 15-7-1478, 15-2-1534).
+date(elzbietaJagiellonkaIII, 1483, 16-2-1517).
+date(jadwigaJagiellonka, 15-3-1513, 7-2-1573).
+date(anna, 1-7-1515, 8-5-1520).
+date(izabelaJagiellonka, 18-1-1519, 15-9-1559).
+date(zofiaJagiellonka, 13-7-1522, 28-5-1575).
+date(annaJagiellonka3, 18-10-1523, 9-9-1596).
+date(katarzynaJagiellonka, 1-11-1526, 16-9-1583).
+
+married(jadwigaAndegawenska, wladyslawIIJagiello).
+married(annaCylejska, wladyslawIIJagiello).
+married(elzbietaGrabowska, wladyslawIIJagiello).
+married(zofiaHolszanska, wladyslawIIJagiello).
+married(elzbietaRakuszanka, kaziemierzIVJagiellonczyk).
+married(barbaraHohenzollern, wladyslawIIJagiellonczyk).
+married(beatryczeAragonska, wladyslawIIJagiellonczyk).
+married(annaDeFoix, wladyslawIIJagiellonczyk).
+married(helenamoskiewska, aleksanderJagiellonczyk).
+married(barbarZapolya, zygmuntIstary).
+married(bonaSforzaDaragona, zygmuntIstary).
+married(mariaHasburzanka, ludwikIIJagiellonczyk).
+married(elzbietaHabsburzanka, zygmuntIIAugust).
+married(barbaraRadziwillowna, zygmuntIIAugust).
+married(katarzynaHabsburzanka, zygmuntIIAugust).
+married(jerzyBogaty, jadwigaJagiellonka2).
+married(fryderykStarszyHohenzollern, zofiaJagiellonka1).
+married(ferdynandIHabsburg, annaJagiellonka1).
+married(boguslawX, annaJagiellonka2).
+married(jerzySaski, barbaraJagiellonka).
+married(fryderykII, elzbietaJagiellonkaIII).
+married(joahimIIHektor, jadwigaJagiellonka).
+married(janZapolyia, izabelaJagiellonka).
+married(henrykII, zofiaJagiellonka).
+married(stefanBatory, annaJagiellonka3).
+married(janIIIWaza, katarzynaJagiellonka).
+
+/*  child (child, parent)  */
+
+child(elzbietaBonifacja, wladyslawIIJagiello).
+child(elzbietaBonifacja, jadwigaAndegawenska).
+child(jadwigaJagiellonka1, wladyslawIIJagiello).
+child(jadwigaJagiellonka1, annaCylejska).
+child(wladyslawIIIWarnenczyk, wladyslawIIJagiello).
+child(wladyslawIIIWarnenczyk, zofiaHolszanska).
+child(kazimierz, wladyslawIIJagiello).
+child(kazimierz, zofiaHolszanska). 
+child(kaziemierzIVJagiellonczyk, wladyslawIIJagiello).
+child(kaziemierzIVJagiellonczyk, zofiaHolszanska). 
+child(wladyslawIIJagiellonczyk, kaziemierzIVJagiellonczyk). 
+child(wladyslawIIJagiellonczyk, elzbietaRakuszanka).
+child(jadwigaJagiellonka2, kaziemierzIVJagiellonczyk).
+child(jadwigaJagiellonka2, elzbietaRakuszanka).
+child(kaziemierzJagiellonczyk, kaziemierzIVJagiellonczyk). 
+child(kaziemierzJagiellonczyk, elzbietaRakuszanka).
+child(janIolbracht, kaziemierzIVJagiellonczyk).
+child(janIolbracht, elzbietaRakuszanka).
+child(aleksanderJagiellonczyk, kaziemierzIVJagiellonczyk).
+child(aleksanderJagiellonczyk, elzbietaRakuszanka).
+child(zofiaJagiellonka1, kaziemierzIVJagiellonczyk).
+child(zofiaJagiellonka1, elzbietaRakuszanka).
+child(elzbietaJagiellonkaI, kaziemierzIVJagiellonczyk).
+child(elzbietaJagiellonkaI, elzbietaRakuszanka).
+child(annaJagiellonka1, wladyslawIIJagiellonczyk).
+child(annaJagiellonka1, annaDeFoix).
+child(ludwikIIJagiellonczyk, wladyslawIIJagiellonczyk).
+child(ludwikIIJagiellonczyk, annaDeFoix).
+child(zygmuntIstary, kaziemierzIVJagiellonczyk).
+child(zygmuntIstary, elzbietaRakuszanka).
+child(fryderykJagiellonczyk, kaziemierzIVJagiellonczyk).
+child(fryderykJagiellonczyk, elzbietaRakuszanka).
+child(elzbietaJagiellonkaII, kaziemierzIVJagiellonczyk).
+child(elzbietaJagiellonkaII, elzbietaRakuszanka).
+child(annaJagiellonka2, kaziemierzIVJagiellonczyk).
+child(annaJagiellonka2, elzbietaRakuszanka).
+child(barbaraJagiellonka, kaziemierzIVJagiellonczyk).
+child(barbaraJagiellonka, elzbietaRakuszanka).
+child(elzbietaJagiellonkaIII, kaziemierzIVJagiellonczyk).
+child(elzbietaJagiellonkaIII, elzbietaRakuszanka).
+child(jadwigaJagiellonka, zygmuntIstary).
+child(jadwigaJagiellonka, barbarZapolya).
+child(anna, zygmuntIstary).
+child(anna, barbarZapolya).
+child(izabelaJagiellonka, zygmuntIstary).
+child(izabelaJagiellonka, bonaSforzaDaragona).
+child(zygmuntIIAugust, zygmuntIstary).
+child(zygmuntIIAugust, bonaSforzaDaragona).
+child(zofiaJagiellonka2, zygmuntIstary).
+child(zofiaJagiellonka2, bonaSforzaDaragona).
+child(annaJagiellonka3, zygmuntIstary).
+child(annaJagiellonka3, bonaSforzaDaragona).
+child(katarzynaJagiellonka, zygmuntIstary).
+child(katarzynaJagiellonka, bonaSforzaDaragona).
+child(wojciechOlbracht, zygmuntIstary).
+child(wojciechOlbracht, bonaSforzaDaragona).
+
+/*  grandchild (grandchild, grandparent)  */
+
+grandchild(wladyslawIIJagiellonczyk, wladyslawIIJagiello).
+grandchild(kaziemierzJagiellonczyk, wladyslawIIJagiello).
+grandchild(janIOlbrach, wladyslawIIJagiello).
+grandchild(aleksanderJagiellonczyk, wladyslawIIJagiello).
+grandchild(ludwikIIJagiellonczyk, kaziemierzIVJagiellonczyk).
+grandchild(zygmuntIstary, wladyslawIIJagiello).
+grandchild(fryderykJagiellonczyk, wladyslawIIJagiello).
+grandchild(zygmuntIIAugust, kaziemierzIVJagiellonczyk).
+grandchild(wojciechOlbracht, kaziemierzIVJagiellonczyk).
+grandchild(jadwigaJagiellonka2, wladyslawIIJagiello).
+grandchild(zofiaJagiellonka1, wladyslawIIJagiello).
+grandchild(elzbietaJagiellonkaI, wladyslawIIJagiello).
+grandchild(annaJagiellonka1, kaziemierzIVJagiellonczyk).
+grandchild(elzbietaJagiellonkaII, wladyslawIIJagiello).
+grandchild(annaJagiellonka2, wladyslawIIJagiello).
+grandchild(barbaraJagiellonka, wladyslawIIJagiello).
+grandchild(elzbietaJagiellonkaIII, wladyslawIIJagiello).
+grandchild(jadwigaJagiellonka, kaziemierzIVJagiellonczyk).
+grandchild(anna, kaziemierzIVJagiellonczyk).
+grandchild(izabelaJagiellonka, kaziemierzIVJagiellonczyk).
+grandchild(zofiaJagiellonka, kaziemierzIVJagiellonczyk).
+grandchild(annaJagiellonka3, kaziemierzIVJagiellonczyk).
+grandchild(katarzynaJagiellonka, kaziemierzIVJagiellonczyk).
+
+
+% Rules
+
+parent(X,Y) :- child(Y,X).
+sibling(X,Y) :- child(X,Z), child(Y,Z), X\= Y.
+ancestor(X,Y) :- parent(X,Y); ( parent(X,Z), ancestor(Z,Y) ).
+descendand(X,Y) :- ancestor(Y,X).
+grandfather(X,Y) :- grandchild(Y,X).
+
+when_they_lived :- date(X,Y,Z), format(' ~w was born in ~w and died in ~w\n', [X,Y,Z]),fail.
+
+wives_of_wladyslawIIJagiello:- married(X,wladyslawIIJagiello), format(' ~w\n',[X]),fail.
+wives_of_wladyslawIIJagiellonczyk:- married(X,wladyslawIIJagiellonczyk), format(' ~w\n',[X]),fail.
+wives_of_zygmuntIstary:- married(X,zygmuntIstary), format(' ~w\n',[X]),fail.
+wife_of_kaziemierzIVJagiellonczyk :- married(X,kaziemierzIVJagiellonczyk), format(' ~w\n',[X]),fail.
+
+children_of_wladyslawIIJagiello :- child(X,wladyslawIIJagiello), format(' ~w\n',[X]),fail.
+children_of_kaziemierzIVJagiellonczyk :- child(X,kaziemierzIVJagiellonczyk),  format(' ~w\n',[X]),fail.
+children_of_wladyslawIIJagiellonczyk :- child(X,wladyslawIIJagiellonczyk), format(' ~w\n',[X]),fail.
+children_of_zygmuntIstary :- child(X,zygmuntIstary), format(' ~w\n',[X]),fail.
+
+grandchildren_of_wladyslawIIJagiello :- grandchild(X,wladyslawIIJagiello), format(' ~w\n',[X]),fail.
+grandchildren_of_kaziemierzIVJagiellonczyk :- grandchild(X,kaziemierzIVJagiellonczyk), format(' ~w\n',[X]),fail.
+
+description :- write('The picture presents the family tree of the Jagiellonian dynasty. They lived in different time frames: '),nl,
+	when_they_lived;
+    write('The head of the dynasty is Wladyslaw II Jagiello '),nl,
+    write('Who had five children: '),nl,
+    children_of_wladyslawIIJagiello,
+    write('He was married four times with the following women: '),nl,
+    wives_of_wladyslawIIJagiello;
+    write('His son, Kaziemierz IV Jagiellonczyk, was married once with: '),nl,
+    wife_of_kaziemierzIVJagiellonczyk;       
+    write('They had thirteen children: '),nl,
+    children_of_kaziemierzIVJagiellonczyk;
+    write('His son Wladyslaw II Jagiellonczyk was married three times with: '),nl,
+    wives_of_wladyslawIIJagiellonczyk;
+    write('And he had two kids with his last wife: '),nl,
+    children_of_wladyslawIIJagiellonczyk;
+    write('Zygmunt I Stary, which was also son of Kaziemierz IV Jagiellonczyk, was married twice with: '),nl,
+    wives_of_zygmuntIstary;
+    write('And he had eight children: '),nl,
+    children_of_zygmuntIstary;
+    write('Kaziemierz IV Jagiellonczyk was the grandafther of: '),nl,
+    grandchildren_of_kaziemierzIVJagiellonczyk;                                           
+    write('The last living person from the Jagiellonian dynasty was Anna Jagiellonka.'),nl.                                                                         
